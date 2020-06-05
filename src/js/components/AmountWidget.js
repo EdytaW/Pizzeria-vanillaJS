813981dc0536a13f98a48d8fbcb739684d105554
Edyta Wojdala <edytawoj095@gmail.com>
2020-06-05 14:48:30 +0200

    Add Class BaseWidget.js

--- src/js/components/AmountWidget.js
+++ src/js/components/AmountWidget.js
@@ -1,66 +1,54 @@
 import {settings, select} from '../settings.js';
+import BaseWidget from './BaseWidget.js';
 
-
-class AmountWidget{
+class AmountWidget extends BaseWidget{
   constructor(element){
+    super(element, settings.amountWidget.defaultValue); //tj konstruktor klasy BaseWidget
     const thisWidget = this;
     thisWidget.getElements(element);
     // thisWidget.value = settings.amountWidget.defaultValue;
-    thisWidget.setValue(settings.amountWidget.defaultValue);
+    // thisWidget.setValue(settings.amountWidget.defaultValue);
     thisWidget.initActions();
     // console.log('AmountWidget:', thisWidget);
     // console.log('constructor argments:', element);
   }
-  getElements(element){
+  getElements(){
     const thisWidget = this;
     
-    thisWidget.element = element;
-    thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
-    thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
-    thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    // thisWidget.dom.wrapper = element;
+    thisWidget.dom.input = thisWidget.dom.wrapper.querySelector(select.widgets.amount.input);
+    thisWidget.dom.linkDecrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkDecrease);
+    thisWidget.dom.linkIncrease = thisWidget.dom.wrapper.querySelector(select.widgets.amount.linkIncrease);
   }
 
-  setValue(value){
+  isValid(value){
+    return !isNaN(value)
+    && value >= settings.amountWidget.defaultMin 
+    && value <= settings.amountWidget.defaultMax;
+  }
+
+  renederValue(){
     const thisWidget = this;
-
-    let newValue = parseInt(value);
-
-    /*TODO: Add validation */
-    if(newValue>=9) {
-      newValue = 9;
-    }else if(newValue <= 1) {
-      newValue = 1;
-    }
-    thisWidget.value = newValue;
-    thisWidget.announce();
-    thisWidget.input.value = thisWidget.value;
+    thisWidget.dom.input.value = thisWidget.value;
   }
 
   initActions(){
     const thisWidget = this;
-    //dla thisWidget.input dodać listener eventu change 
-    thisWidget.input.addEventListener('change', function () {
+    //dlathisWidget.dom.input dodać listener eventu change 
+    thisWidget.dom.input.addEventListener('change', function () {
       //dla którego handler użyje metody setValue z takim samym argumentem, jak w konstruktorze (czyli z wartością inputa)
-      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.value = thisWidget.dom.input.value;
     });
-    thisWidget.linkDecrease.addEventListener('click', function (event) {
+    thisWidget.dom.linkDecrease.addEventListener('click', function (event) {
       event.preventDefault();
       thisWidget.setValue(thisWidget.value - 1);
     });
-    thisWidget.linkIncrease.addEventListener('click', function (event) {
+    thisWidget.dom.linkIncrease.addEventListener('click', function (event) {
       event.preventDefault();
       thisWidget.setValue(thisWidget.value + 1);
     });
   }
 
-  announce(){
-    const thisWidget = this;
-
-    // const event = new Event('updated');
-    const event = new CustomEvent('updated',{
-      bubbles: true
-    });
-    thisWidget.element.dispatchEvent(event);
-  }
+ 
 }
 export default AmountWidget;
