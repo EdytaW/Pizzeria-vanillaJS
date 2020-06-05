860662f22a810469d4aa4affb1a28f7b5083706c
Edyta Wojdala <edytawoj095@gmail.com>
2020-06-05 16:57:10 +0200

    Add datePicker.js (error)

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,6 @@
 import {templates, select} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
-
+import { DatePicker } from './DatePicker.js';
 export class Booking{
   constructor(element){
     const thisBooking = this; 
@@ -16,12 +16,14 @@ export class Booking{
     thisBooking.dom.wrapper.innerHTML = generatHTML;
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = thisBooking.dom.wrapper.querySelector(select.widgets.datePicker.wrapper);
   } 
   
   initWidgets(){
     const thisBooking = this; 
     thisBooking.peopleAmount = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.hoursAmount = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
   }
 }
 export default Booking;
